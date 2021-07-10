10a7a02e98fab8a708384a01ee23653f18b187fa
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-10 13:19:08 +0200

    route descriptiions

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('the main route is working')
+  res.json(
+    {
+      "/pharmacies": "returns pharmacies list for stratford",
+      "/colleges": "returns colleges list for stratford",
+      "/doctors":	"returns doctors list for stratford",
+      "/hospitals":	"returns hospitals list for stratford"
+    }
+  )
 });
 
 app.listen(PORT, () => {
