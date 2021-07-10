78158e3197dc103850e1d8020e2971e04497cb81
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-10 12:25:17 +0200

    main route is working

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.get('/', (req, res) => {
+  res.send('the main route is working')
+});
+
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`)
 });
