52817955127d8364666cb262b221be165314662b
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-25 20:09:20 +0200

    install cors middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(cors);
 
 // Json files for Cities
 const Stratford = require('./Stratford.json');
