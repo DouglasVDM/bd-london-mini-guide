87430201aa4f7145a7824861c65be26b589d4786
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-28 14:48:45 +0200

    dynamic category route

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 // Json files for Cities
 const Stratford = require('./Stratford.json');
@@ -12,11 +17,11 @@ const Heathrow = require('./Heathrow.json');
 const Harrow = require('./Harrow.json');
 
 // Global Cities Variable
-const cities = [
-  { name: "Harrow", data: Harrow },
-  { name: "Heathrow", data: Heathrow },
-  { name: "Stratford", data: Stratford },
-];
+const cities = {
+  harrow: Harrow,
+  heathrow: Heathrow,
+  stratford: Stratford
+};
 
 // Route Descriptions
 app.get('/', (req, res) => {
@@ -34,78 +39,23 @@ app.get('/', (req, res) => {
 
 // List of All the Cities in an Array
 app.get('/cities', (req, res) => {
-  const cityNames = cities.map(city => city.name);
+  const cityNames = Object.keys(cities)
   res.json(cityNames);
 });
   
-  
 // Get All Pharmacies for the City
-app.get('/:city/pharmacies', (req, res) => {
-  const { city } = req.params;
-
-  //  Check if City Exists
-  const foundCity = cities.some(someCity => someCity.name.toLowerCase() === city.toLowerCase());
-
-  //  Return the Pharmacies for the City
-  if (foundCity) {
-    cities.filter((cityInArray) => {
-      if (cityInArray.name.toLowerCase() === city.toLowerCase())
-        (res.json(cityInArray.data.pharmacies))
-    })
-  } else { res.status(400).send(`City: ${city} not found!`) }
+app.get('/:city/:category', (req, res) => {
+  const { city, category } = req.params;
+  
+  // Selected City Validation
+  if (!cities[city]) {
+    res.status(400).send(`City: ${city} not found!`)
+  } if (!cities[city][category]) {
+    res.status(400).send(`Category: ${category} not found!`)
+  } else {
+    res.send(cities[city][category])
+  }
 });
-
-
-// Get All Colleges for the City
-app.get('/:city/colleges', (req, res) => {
-  const { city } = req.params;
-
-  //  Check if City Exists
-  const foundCity = cities.some(someCity => someCity.name.toLowerCase() === city.toLowerCase());
-
-  //  Return the Colleges for the City
-  if (foundCity) {
-    cities.filter((cityInArray) => {
-      if (cityInArray.name.toLowerCase() === city.toLowerCase())
-        (res.json(cityInArray.data.colleges))
-    })
-  } else { res.status(400).send(`City: ${city} not found!`) }
-});
-
-
-// Get All Doctors for the City
-app.get('/:city/doctors', (req, res) => {
-  const { city } = req.params;
-
-  //  Check if City Exists
-  const foundCity = cities.some(someCity => someCity.name.toLowerCase() === city.toLowerCase());
-
-  //  Return the Doctors for the City
-  if (foundCity) {
-    cities.filter((cityInArray) => {
-      if (cityInArray.name.toLowerCase() === city.toLowerCase())
-        (res.json(cityInArray.data.doctors))
-    })
-  } else { res.status(400).send(`City: ${city} not found!`) }
-});
-
-
-// Get All Hospitals for the City
-app.get('/:city/hospitals', (req, res) => {
-  const { city } = req.params;
-
-  //  Check if City Exists
-  const foundCity = cities.some(someCity => someCity.name.toLowerCase() === city.toLowerCase());
-
-  //  Return the Hospitals for the City
-  if (foundCity) {
-    cities.filter((cityInArray) => {
-      if (cityInArray.name.toLowerCase() === city.toLowerCase())
-        (res.json(cityInArray.data.hospitals))
-    })
-  } else { res.status(400).send(`City: ${city} not found!`) }
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`)
